fb90d789ddef356c11a584176c50e65b4ac0c382
Dolimov Khumoyunmirzo| <humodalim@gmail.com>
2020-03-16 22:31:13 +0500

    minor bug fix

--- src/routes/main-page/index.js
+++ src/routes/main-page/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Scanner from "./organisms/Scanner";
 
-const Main = () => {
-  return <Scanner />;
+const Main = props => {
+  return <Scanner history={props.history} />;
 };
 
 export default Main;
--- src/routes/main-page/organisms/Scanner.jsx
+++ src/routes/main-page/organisms/Scanner.jsx
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 import QrReader from "react-qr-reader";
 import { sendPostId } from "../api";
 
-const Scanner = props => {
+const Scanner = ({ history }) => {
   const [result, setResult] = useState("");
 
   const handleScan = data => {
     if (data) {
-      // alert(data);
+      alert(data);
       let post_id = data.split("=")[1];
       setResult(data);
-      sendPostId(post_id, props.history);
+      sendPostId(post_id, history);
     }
   };
   const handleError = err => {
     console.error(err);
   };
+  console.log(history);
   return (
     <div>
       <QrReader
