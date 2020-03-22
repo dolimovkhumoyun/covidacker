8d4e73bf74518d83d1d9961c322343e3a03fa6c2
Dolimov Khumoyunmirzo <humodalim@gmail.com>
2020-03-22 17:05:00 +0500

    add public _redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import Login from "./routes/login";
 
 const App = () => {
   return (
